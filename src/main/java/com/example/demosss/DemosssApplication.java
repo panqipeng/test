a27ca364d96dd3e5d2ae22a4ab10dd15b90edce5
panqipeng <361425215@qq.com>
2021-07-03 18:33:19 +0800

    ff

---
+++ src/main/java/com/example/demosss/DemosssApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemosssApplication.class, args);
+    }
+
+}
